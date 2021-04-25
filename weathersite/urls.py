ef9956daa29e8a90164e4b76d7751eacae9cf69d
gpfforte <gpf_forte@hotmail.com>
2021-04-25 21:51:18 +0200

    25-04-2021

--- weathersite/urls.py
+++ weathersite/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pollsapp/', include('pollsapp.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/pollsapp/', permanent=False)),
 ]
